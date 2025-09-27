aec7b71338ab5ced3c11e975753fe426f2297e9e
Roope Mantere <roope.mantere12@gmail.com>
2025-09-27 22:05:00 +0300

    add dir and file check

--- storage/app.py
+++ storage/app.py
@@ -1,7 +1,24 @@
 from flask import Flask
+import os, pathlib
 
-app = Flask(__name__)
+def startup_tasks():
+    """
+    Needed tasks to check if data directory exists and inside 
+    is a app.log file. Creates new directory and file if the
+    file and dir don't exist
+    """
+    data_dir = pathlib.Path(os.getenv("DATA_DIR", "/data"))
+    log_file = data_dir / "app.log"
+    data_dir.mkdir(parents=True, exist_ok=True)
+    log_file.parent.mkdir(parents=True, exist_ok=True) 
+    log_file.touch(exist_ok=True)
 
-@app.route("/home")
-def hello_world():
-    return "<p>Hello, World!<p>"
+def create_app():
+    """
+    Creates the flask applicaiton.
+    """
+    app = Flask(__name__)
+    startup_tasks()
+    return app
+
+app = create_app()
