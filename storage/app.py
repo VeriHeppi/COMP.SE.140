d3eaefdd47c2c5a81f0b7c65abaefb5d1010dcc4
Roope Mantere <roope.mantere12@gmail.com>
2025-09-29 23:01:49 +0300

    Storage service done

--- storage/app.py
+++ storage/app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-import os, pathlib
+from flask import Flask, request, Response
+import pathlib, os
 
 def startup_tasks():
     """
@@ -7,6 +7,7 @@ def startup_tasks():
     is a app.log file. Creates new directory and file if the
     file and dir don't exist
     """
+    global log_file
     data_dir = pathlib.Path(os.getenv("DATA_DIR", "/data"))
     log_file = data_dir / "app.log"
     data_dir.mkdir(parents=True, exist_ok=True)
@@ -22,3 +23,32 @@ def create_app():
     return app
 
 app = create_app()
+
+@app.route('/log', methods=['POST'])
+def inject_log():
+
+    if(request.content_type != 'text/plain'):
+        return Response("Content-Type must be text/plain", status=415)
+
+    data = request.args.get("timestamp")
+    data = data.rstrip()
+    if(len(data) == 0):
+        return Response("Timestamp value is empty", status=400)
+    
+    with (log_file, 'a') as file:
+        file.write(f"{data}\n")
+    return Response("Log updated", status=204)
+
+@app.route('/log', methods=['GET'])
+def read_log():
+    try:
+        with open(log_file, 'r') as file:
+            content = file.read()
+        return Response(content,
+                        mimetype='text/plain',
+                        status=200)
+    except FileNotFoundError:
+        return Response("Log file not found", status=404)
+    except Exception  as e:
+        return Response(f"Error reading the file: {e}", status=500)
+        
